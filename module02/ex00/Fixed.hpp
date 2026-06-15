100695c8c0bced0bb051d235d6f1b90c15e98af1
白石大生 <shiraishidaisei@shiraishidaiseinoMacBook-Air.local>
2026-06-15 13:54:04 +0200

    start module02

---
+++ module02/ex00/Fixed.hpp
@@ -0,0 +1,17 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+    private:
+        int value;
+        static const int bits;
+    
+    public:
+        Fixed():value(0){};
+        
+}
+
+#endif
