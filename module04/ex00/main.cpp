1f37b4c4b159deb4cba93e2a6e8b38206b492b40
白石大生 <shiraishidaisei@shiraisseinoAir.telekom.ip>
2026-07-13 11:54:41 +0200

    module04

---
+++ module04/ex00/main.cpp
@@ -0,0 +1,40 @@
+#include "Animal.hpp"
+
+int main()
+{
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+
+    std::cout << std::endl;
+
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound(); //will output the cat sound!
+    j->makeSound();
+    meta->makeSound();
+
+    std::cout << std::endl;
+
+    delete j;
+    delete i;
+    delete meta;
+
+    std::cout << std::endl;
+
+    const WrongAnimal* fmeta =new WrongAnimal();
+    const WrongAnimal* k = new WrongCat();
+
+    std::cout << std::endl;
+
+    std::cout << k->getType() << " " << std::endl;
+    k->makeSound();
+    fmeta->makeSound();
+
+    std::cout << std::endl;
+
+    delete k;
+    delete fmeta;
+
+    return 0;
+}
