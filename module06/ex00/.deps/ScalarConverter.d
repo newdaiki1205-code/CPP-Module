d2d61eb0586b14a43eb992aaeb7177a08046701b
白石大生 <shiraishidaisei@shiraishidaiseinoMacBook-Air.local>
2026-09-12 12:37:48 +0200

    cpp06 done

--- module06/ex00/.deps/ScalarConverter.d
+++
@@ -1,4 +0,0 @@
-obj/ScalarConverter.o: src/ScalarConverter.cpp \
-  src/../include/ScalarConverter.hpp
-
-src/../include/ScalarConverter.hpp:
--- module06/ex00/.deps/main.d
+++
@@ -1,3 +0,0 @@
-obj/main.o: src/main.cpp src/../include/ScalarConverter.hpp
-
-src/../include/ScalarConverter.hpp:
